cf50c1d67acf5599b3a31d188579a0566500e9cd
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2019-08-28 16:36:02 -0500

    removed modal popup. changed it to delete prompt on clicking an individual event

--- src/App.js
+++ src/App.js
@@ -15,12 +15,21 @@ class App extends Component {
       events 
    };
     this.handleSelect = this.handleSelect.bind(this);
-    this.editEvent = this.editEvent.bind(this);
+    this.deleteEvent = this.deleteEvent.bind(this);
   }
 
   componentDidMount() {
+    // listen for the calendar to fully load, then show notifications for the day's events
+    window.addEventListener('load', this.handleLoad);
+  }
+
+  handleLoad = () => {
+    //check for any event starting at the beginning of today
     let todayStart = new Date().setHours(0,0,0,0);
+    //check for any event end at the end of today
     var todayEnd = new Date().setHours(23,59,59,999);
+
+    //for each event that is happening today, fire off a notification for that event with the details
     this.state.events.forEach(event => {
       if(event['start'].getTime() >= todayStart && event['start'].getTime() <= todayEnd){
         return alert(event.title + "\nStarts: " + event.start + "\nEnds: " + event.end)
@@ -28,35 +37,45 @@ class App extends Component {
     })
   }
 
-  editEvent = ({ start, end, title }) => {
-    // Get the modal
-    var modal = document.getElementById("myModal");
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+  deleteEvent = (event, { start, end, title }) => {
+    // // Get the modal
+    // var modal = document.getElementById("myModal");
+    // // Get the button that opens the modal
+    // var btn = document.getElementById("myBtn");
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
 
-    var message = document.getElementsByClassName("message")[0];
+    // var message = document.getElementsByClassName("message")[0];
 
-    // this.props.eventMessage = title;
-    console.log(title);
+    // // this.props.eventMessage = title;
+    // console.log(title);
 
-    // When the user clicks the button, open the modal 
-    modal.style.display = "block";
-    message.innerHTML = title;
+    // // When the user clicks the button, open the modal 
+    // modal.style.display = "block";
+    // message.innerHTML = title + "<br/> Start: " + start + "<br/> End: " + end
+    // + "<br/><button>Edit</button> <button onclick={deleteEvent}>Delete</button>";
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-    // calendar.getEventById( id ).remove()
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    //   }
+    // }
+    const r = window.confirm("Would you like to remove this event?")
+    if(r === true){
+     
+    this.setState((prevState, props) => {
+      const events = [...prevState.events]
+      const idx = events.indexOf(event)
+      events.splice(idx, 1);
+      return { events };
+    });
+   }
   }
 
   handleSelect = ({ start, end }) => {
@@ -105,12 +124,12 @@ class App extends Component {
     
     return (
       <div className="App">
-        <div id="myModal" className="modal">
+        {/* <div id="myModal" className="modal">
           <div className="modal-content">
             <span className="close">&times;</span>
             <p className="message"></p>
           </div>
-        </div>
+        </div> */}
         {/* <Calendar
           localizer={localizer}
           defaultDate={new Date()}
@@ -125,6 +144,7 @@ class App extends Component {
           </strong>
         </ExampleControlSlot.Entry> */}
         <Calendar
+          popup
           selectable
           localizer={localizer}
           events={this.state.events}
@@ -132,7 +152,7 @@ class App extends Component {
           scrollToTime={new Date(1970, 1, 1, 6)}
           // defaultDate={new Date(2015, 3, 12)}
           // onSelectEvent={event => alert(event.title + "\n" +  event.start + "\n" + event.end)}
-          onSelectEvent={this.editEvent}
+          onSelectEvent={this.deleteEvent}
           onSelectSlot={this.handleSelect}
           style={{ height: "90vh" }}
         />
